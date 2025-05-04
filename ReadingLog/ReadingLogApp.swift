715c0df9c814529e5736b4ea6ea243677fe7eeb5
Joan <joanm95@gmail.com>
2025-05-04 19:24:52 -0400

    Created AddBookView and Book.swift

--- ReadingLog/ReadingLogApp.swift
+++ ReadingLog/ReadingLogApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ReadingLogApp: App {
@@ -13,5 +14,6 @@ struct ReadingLogApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
